b5d27f35eac18778ac91890d845f563dd2339508
urielja <167566939+urielja@users.noreply.github.com>
2025-05-31 13:01:31 -0600

    Operador aritmeticos, de comparacion etc
    Ejercicio de operadores

---
+++ fundamentos.py
@@ -0,0 +1,21 @@
+if __name__== "__main__":
+
+    print("-----Operador aritmeticos-----")
+    print("Operador suma, 5+6",5+6)
+    print("Operador resta, 9-8",9-8)
+    print("Operador multiplicacion, 4*4",4*4)
+    print("Operador potencia, 6**2",6**2)
+    print("Operador division, 20/4",20/4)
+    print("Operador division sin decimal, 20//4",20//4)
+    print("Operador modulo, 20%8",20%8)
+    print("Operador cambio de signo, -20",-20)
+    
+    print("-----Operador Logicos-----")
+    print("True and True",True and True)
+    print("True or False",True or False)
+    print("-----Operador de comparacion-----")
+    print("5 == 5",5 == 5)    
+    #Comentario
+
+    '''Comentario con 
+    Multiples lines'''
